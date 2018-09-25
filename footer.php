e466134918c86709305f5ca333b30abe02263645
Rafael Muto <r.nagahama@gmail.com>
2018-09-25 13:06:59 -0300

    ajustes no sistema de grid e no footer

--- footer.php
+++ footer.php
@@ -5,11 +5,7 @@
   <div class="footer_msg">
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
   </div>
-  <div class="footer_icons">
-    <i class="material-icons" style="font-size:40px;">add_circle_outline</i>
-    <i class="material-icons" style="font-size:40px;">add_circle_outline</i>
-    <i class="material-icons" style="font-size:40px;">add_circle_outline</i>
-  </div>
+
 </footer>
 
 <div class="footer_nav">
