9d110a217ca5e7be654ce83426765e7a4dfcc7b3
alinessilva <alinedoze@gmail.com>
2018-09-28 17:26:54 -0300

    Desenvolvimento inicial da página do produto

--- product.php
+++ product.php
@@ -18,72 +18,65 @@
     <?php include("header.php") ?>
 
     <!-- ==== MAIN ==== -->
-      <ul class="product-grid">
-        <li class="main_card product-card">
-          <h2>Nome do Vendedor</h2>
-          <figure>
-            <img src="./imagens/avatar.png" alt="">
-          </figure>
-          <div>
-            <p>Informações do Vendedor</p>
-          </div>
-          <div>
-            <a href="">Math</a>
-          </div>
-        </li>
-        <li class="main_card wide">
-          <h2>Nome do Produto</h2>
-          <figure>
-            <img class="product_card--img" src="./imagens/produto1.jpg" alt="">
-          </figure>
-          <div>
-            <h4>Detalhes do Produto</h4>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit beatae optio!</p>
-          </div>
-        </li>
-      </ul>
+    <div class="container">
+      <nav class="sidenav">
+        <ul>
+          <li><a href="#"><img src="imagens/produto2.jpg" alt="Produto"></a></li>
+          <li><a href="#"><img src="imagens/produto3.jpg" alt="Produto"></a></li>
+          <li><a href="#"><img src="imagens/produto4.jpg" alt="Produto"></a></li>
+          <li><a href="#"><img src="imagens/produto5.jpg" alt="Produto"></a></li>
+        </ul>
+      </nav>
 
-      <h3>Você também pode se interessar...</h3>
-      <ul class="main_grid">
-      
-      <li class="main_card">
-          <figure>
-            <img class="main_card--img hover-opacity" src="./imagens/produto10.jpg" alt="sapatilha">
-          </figure>
-          <div class="main_card--info">
-            <div>
-              <p>Nome do Produto</p>
-              <p class="price">R$ 10,00</p>
-            </div>
-            <a href="#"><i class="material-icons">thumb_up</i></a>
+      <main class="content">
+          <div class="title">
+            <h1>Lojinha da Ana</h1>
+            <span>desapegando</span>
           </div>
-        </li>
-        <li class="main_card">
-          <figure>
-            <img class="main_card--img hover-opacity" src="./imagens/produto11.jpg" alt="sapatilha">
-          </figure>
-          <div class="main_card--info">
+          
+          <div class="link">
             <div>
-              <p>Nome do Produto</p>
-              <p class="price">R$ 10,00</p>
+              <span class="link_img"><img src="imagens/avatar.png" alt="avatar"></span>
+              <a class="link_click">Perfil</a>
             </div>
-            <a href="#"><i class="material-icons">thumb_up</i></a>
-          </div>
-        </li>
-        <li class="main_card">
-          <figure>
-            <img class="main_card--img hover-opacity" src="./imagens/produto12.jpg" alt="sapatilha">
-          </figure>
-          <div class="main_card--info">
             <div>
-              <p>Nome do Produto</p>
-              <p class="price">R$ 10,00</p>
+              <span class="link_img"><img src="imagens/loja.png" alt="loja"></span>
+              <a class="link_click">Seguir</a>
             </div>
-            <a href="#"><i class="material-icons">thumb_up</i></a>
           </div>
-      </ul>
-    <!-- ==== FOOTER ==== -->
-        <?php include("footer.php") ?>
+
+            <h2 class="title col-wide">Pulseira Super Estilosa!</h2>
+            <img class="col-wide" src="imagens/produto8.jpg" alt="Produto">
+            <button class="btn-products col-wide">MATCH</button>
+          
+            <ul class="info-products">
+              <li>Condição: </li>
+              <li>Tamanho: </li>
+              <li>Cor: </li>
+              <li>Marca: </li>
+            </ul>
+
+          <div class="info">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt beatae recusandae delectus dolorem aperiam harum nemo explicabo deserunt assumenda, commodi eligendi labore vitae nisi deleniti enim provident consequatur, natus ad!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quasi ea quidem cumque sint necessitatibus corporis eum provident adipisci porro id quaerat, autem repellat architecto accusamus inventore commodi fugiat fuga.</p>
+          </div>
+          
+        </main>
+        
+        <aside class="shop">
+          <h4 class="title">Você também pode gostar...</h4>
+          <div class="shop-item">
+            <img src="imagens/produto11.jpg" alt="Produto">
+          </div>
+          
+          <div class="shop-item">
+            <img src="imagens/produto12.jpg" alt="Produto">
+          </div>
+        </aside>
+    </div>
+
+      <!-- ==== FOOTER ==== -->
+      <?php include("footer.php") ?>
   </body>
 
 </html>
