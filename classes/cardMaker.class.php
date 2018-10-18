2c8fd7d395a2b999912c74b99b8877c64fde7e00
Rafael Muto <r.nagahama@gmail.com>
2018-10-18 12:01:11 -0300

    correcoes e ajustes nas classes

--- classes/cardMaker.class.php
+++ classes/cardMaker.class.php
@@ -6,28 +6,24 @@ include_once "cadastroProduto.class.php";
  */
 class cardMaker extends cadastroProduto
 {
-  protected $produto_id;
-  protected $usuario_id;
-  protected $nome;
-  protected $descricao;
-  protected $imagem;
-  protected $data;
-  protected $status_id;
-  protected $valor;
 
   protected $pdo;
 
   public function printCard($i){
     $info = $this->getInfo($i);
-    $this->produto_id = $info["produto_id"];
-    $this->usuario_id = $info["usuario_id"];
-    $this->nome = $info["nome"];
-    $this->descricao = $info["descricao"];
-    $this->imagem = $info["imagem"];
-    $this->data = $info["data"];
-    $this->status_id = $info["status_id"];
-    $this->valor = $info["valor"];
-    echo '<div class="main_card hover-opacity efx_drop_shadow efx_border_radius" style="background: url(' . $this->imagem . ');"><div class="main_card_info"><p>' . $this->nome . '</p><p>R$ ' . $this->valor . '</p></div><a class="like_btn" href=" #"><i class="like_btn_icon material-icons">thumb_up</i></a><a class="main_card_link" href="produto.php?produto_id=' . $this->produto_id . '"></a></div>';
+    $produto_id = $info["produto_id"];
+    $usuario_id = $info["usuario_id"];
+    $nome = $info["nome"];
+    $descricao = $info["descricao"];
+    $imagem = $info["imagem"];
+    $data = $info["data"];
+    $status_id = $info["status_id"];
+    $valor = $info["valor"];
+    echo '<div class="main_card hover-opacity efx_drop_shadow efx_border_radius" style="background: url(' . $imagem . ');"><div class="main_card_info"><p>' . $nome . '</p><p>R$ ' . $valor . '</p></div><a class="like_btn" href=" #"><i class="like_btn_icon material-icons">thumb_up</i></a><a class="main_card_link" href="produto.php?produto_id=' . $produto_id . '"></a></div>';
+  }
+
+  public function pageInfo($id){
+    return $this->getInfo($id);
   }
 
 }
