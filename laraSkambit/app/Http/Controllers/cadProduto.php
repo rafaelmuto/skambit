d3f8e6c7babe5809c3b3236c3e5f86dfcf25b002
Rafael Muto <r.nagahama@gmail.com>
2018-11-30 16:15:18 -0200

    limpeza de codigo

--- laraSkambit/app/Http/Controllers/cadProduto.php
+++ laraSkambit/app/Http/Controllers/cadProduto.php
@@ -7,34 +7,32 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 
 class cadProduto extends Controller{
-        
-    public function add(Request $req){
 
-          if($req->isMethod('GET')){
-          return view('cadProduto');
-        }
-          if($req->session()->get('usuario_id') === null){
-            return redirect('home');
-        }
-        
-        $login = $req->session()->get('login');
-        $imagem = $this->setImagem($req->input('imagem'));
-        $usuario_id = $req->session()->get('usuario_id');
-        $prod_id = DB::table('cad_produto')->insert(["nome"=>$req->input('nome'),
-                                    "descricao"=>$req->input('descricao'),
-                                    "valor"=>$req->input('valor'),
-                                    "imagem"=>$imagem,
-                                    "usuario_id"=>$usuario_id,
-                                    "status_id"=>1
-                                    ]);
-            return view('cadProduto');
+  public function add(Request $req){
+    if($req->isMethod('GET')){
+      return view('cadProduto');
+    }
+    if($req->session()->get('usuario_id') === null){
+      return redirect('home');
+    }
+    $login = $req->session()->get('login');
+    $imagem = $this->setImagem($req->input('imagem'));
+    $usuario_id = $req->session()->get('usuario_id');
+    $prod_id = DB::table('cad_produto')->insert(["nome"=>$req->input('nome'),
+                                                 "descricao"=>$req->input('descricao'),
+                                                 "valor"=>$req->input('valor'),
+                                                 "imagem"=>$imagem,
+                                                 "usuario_id"=>$usuario_id,
+                                                 "status_id"=>1
+                                               ]);
+    return view('cadProduto');
   }
-  
+
   private function setImagem(){
      if(!is_dir("imagem/")){
       mkdir("imagem/");
   }
-     
+
     if(isset($_FILES["imagem"]) && $_FILES["imagem"]["error"]==UPLOAD_ERR_OK){
     $timestamp = date("YmdHis");
     $file_ext = explode(".", strtolower($_FILES["imagem"]["name"]));
@@ -46,7 +44,7 @@ class cadProduto extends Controller{
     return false;
     }
   }
-  
+
   public function update($prod_id, Request $req){
     if($req->session()->get('usuario_id') == null){
       return redirect('home');
@@ -64,39 +62,37 @@ class cadProduto extends Controller{
     }
 
     DB::table('cad_produto')->where('produto_id', $prod_id)->update(["nome"=>$req->input('nome'),
-                                                                                            "descricao"=>$req->input('descricao'),
-                                                                                            "valor"=>$req->input('valor'),
-                                                                                            "imagem"=>$imagem
-                                                                                            ]);
+                                                                     "descricao"=>$req->input('descricao'),
+                                                                     "valor"=>$req->input('valor'),
+                                                                     "imagem"=>$imagem
+                                                                    ]);
     return redirect('home');
-  } 
-  
+  }
+
   public function delete($prod_id, Request $req){
     if($req->session()->get('usuario_id') == null){
       return redirect('home');
   }
-  
+
   $delproduto = DB::table('cad_produto')->where('produto_id', '=', $prod_id)->get();
   if($req->isMethod('GET')){
-   return view('delProduto',["delproduto"=>$delproduto,]);
-   //return var_dump($delproduto);
- }
+    return view('delProduto',["delproduto"=>$delproduto,]);
+  }
 
- $imagem = $this->setImagem($req->input('usuario_id'));
- if($imagem==false){
- $imagem = $delproduto[0]->imagem;
- }
- 
- $usuario_id = $req->session()->get('usuario_id');
- DB::table('cad_produto')->where('produto_id', $prod_id)->update(["nome"=>$req->input('nome'),
-                                                                                         "descricao"=>$req->input('descricao'),
-                                                                                         "valor"=>$req->input('valor'),
-                                                                                         "imagem"=>$imagem,
-                                                                                         "usuario_id"=>$usuario_id,
-                                                                                         "status_id"=>2
-                                                                                         ]);
- return redirect('home');
-}
+  $imagem = $this->setImagem($req->input('usuario_id'));
+  if($imagem==false){
+    $imagem = $delproduto[0]->imagem;
+  }
+
+  $usuario_id = $req->session()->get('usuario_id');
+  DB::table('cad_produto')->where('produto_id', $prod_id)->update(["nome"=>$req->input('nome'),
+                                                                   "descricao"=>$req->input('descricao'),
+                                                                   "valor"=>$req->input('valor'),
+                                                                   "imagem"=>$imagem,
+                                                                   "usuario_id"=>$usuario_id,
+                                                                   "status_id"=>2
+                                                                 ]);
+  return redirect('home');
+  }
 
 }
-    
