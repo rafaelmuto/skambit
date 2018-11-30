936a75cc78b0df352132c6ea4ad3b046fd415372
Thiago <thiago.pereira@ticketstore.com.br>
2018-11-30 10:00:35 -0200

    Inclusão da função delete no cadProduto

--- laraSkambit/app/Http/Controllers/cadProduto.php
+++ laraSkambit/app/Http/Controllers/cadProduto.php
@@ -7,44 +7,95 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 
 class cadProduto extends Controller{
-
+        
     public function add(Request $req){
-      if($req->isMethod('GET')){
-      return view('cadProduto');
-      }
-      if($req->session()->get('usuario_id') === null){
-          return redirect('home');
-      }
+          if($req->isMethod('GET')){
+          return view('cadProduto');
+        }
+          if($req->session()->get('usuario_id') === null){
+            return redirect('home');
+        }
+        
+        $login = $req->session()->get('login');
+        $imagem = $this->setImagem($req->input('imagem'));
+        $usuario_id = $req->session()->get('usuario_id');
+        $prod_id = DB::table('cad_produto')->insert(["nome"=>$req->input('nome'),
+                                    "descricao"=>$req->input('descricao'),
+                                    "valor"=>$req->input('valor'),
+                                    "imagem"=>$imagem,
+                                    "usuario_id"=>$usuario_id,
+                                    "status_id"=>1
+                                    ]);
+            return view('cadProduto');
+  }
+  
+  private function setImagem(){
+     if(!is_dir("imagem/")){
+      mkdir("imagem/");
+  }
+     
+    if(isset($_FILES["imagem"]) && $_FILES["imagem"]["error"]==UPLOAD_ERR_OK){
+    $timestamp = date("YmdHis");
+    $file_ext = explode(".", strtolower($_FILES["imagem"]["name"]));
+    $file_name = $timestamp."_".".".$file_ext[1];
+    $file = $_FILES["imagem"]["tmp_name"];
+    move_uploaded_file($file, "imagem/".$file_name);
+    return "imagem/".$file_name;
+  }else{
+    return false;
+    }
+  }
+  
+  public function update($prod_id, Request $req){
+    if($req->session()->get('usuario_id') == null){
+      return redirect('home');
+  }
 
-      $login = $req->session()->get('login');
-      $imagem = $this->setImagem($req->input('imagem'));
-      $usuario_id = $req->session()->get('usuario_id');
-      $prod_id = DB::table('cad_produto')->insert(["nome"=>$req->input('nome'),
-                                                   "descricao"=>$req->input('descricao'),
-                                                   "valor"=>$req->input('valor'),
-                                                   "imagem"=>$imagem,
-                                                   "usuario_id"=>$usuario_id,
-                                                   "status_id"=>1
-                                                  ]);
-      return view('cadProduto');
-      }
+    $upproduto = DB::table('cad_produto')->where('produto_id', '=', $prod_id)->get();
+     if($req->isMethod('GET')){
+      return view('upProduto',["upproduto"=>$upproduto,]);
+      //return var_dump($upproduto);
+    }
 
-     private function setImagem(){
-       if(!is_dir("imagem/")){
-         mkdir("imagem/");
-       }
+    $imagem = $this->setImagem($req->input('usuario_id'));
+    if($imagem==false){
+    $imagem = $upproduto[0]->imagem;
+    }
 
-       if(isset($_FILES["imagem"]) && $_FILES["imagem"]["error"]==UPLOAD_ERR_OK){
-         $timestamp = date("YmdHis");
-         $file_ext = explode(".", strtolower($_FILES["imagem"]["name"]));
-         $file_name = $timestamp."_".".".$file_ext[1];
-         $file = $_FILES["imagem"]["tmp_name"];
-         move_uploaded_file($file, "imagem/".$file_name);
-         return "imagem/".$file_name;
-       }
-       else{
-         return false;
-       }
-     }
+    DB::table('cad_produto')->where('produto_id', $prod_id)->update(["nome"=>$req->input('nome'),
+                                                                                            "descricao"=>$req->input('descricao'),
+                                                                                            "valor"=>$req->input('valor'),
+                                                                                            "imagem"=>$imagem
+                                                                                            ]);
+    return redirect('home');
+  } 
+  
+  public function delete($prod_id, Request $req){
+    if($req->session()->get('usuario_id') == null){
+      return redirect('home');
+  }
+  
+  $delproduto = DB::table('cad_produto')->where('produto_id', '=', $prod_id)->get();
+  if($req->isMethod('GET')){
+   return view('delProduto',["delproduto"=>$delproduto,]);
+   //return var_dump($delproduto);
+ }
+
+ $imagem = $this->setImagem($req->input('usuario_id'));
+ if($imagem==false){
+ $imagem = $delproduto[0]->imagem;
+ }
+ 
+ $usuario_id = $req->session()->get('usuario_id');
+ DB::table('cad_produto')->where('produto_id', $prod_id)->update(["nome"=>$req->input('nome'),
+                                                                                         "descricao"=>$req->input('descricao'),
+                                                                                         "valor"=>$req->input('valor'),
+                                                                                         "imagem"=>$imagem,
+                                                                                         "usuario_id"=>$usuario_id,
+                                                                                         "status_id"=>2
+                                                                                         ]);
+ return redirect('home');
+}
 
 }
+    
